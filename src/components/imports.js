07c99e0a6fb4fe8b9f626cfc89ea8765fbdb5627
Wiktor Bandzyra <werstaptor@gmail.com>
2022-07-13 19:32:58 +0200

    Added useMediaQueyHook, refactored imports

--- src/components/imports.js
+++ src/components/imports.js
@@ -1,2 +1,29 @@
+import logo from '../assets/l.png'
+import zamow from '../assets/zamow_ikona.svg'
+import mojekonto from '../assets/mojekonto.svg'
+import koszyk from '../assets/koszyk.svg'
+import slider1 from '../assets/slider1.png'
+import Pyszne from '../assets/Pyszne.png'
+import DeliGoo from '../assets/DeliGoo.png'
+import uber_eats from '../assets/uber_eats.png'
+import Glovo from '../assets/Glovo.png'
+import bolt_food from '../assets/bolt_food.png'
+
 export { default as Navbar } from './navbar/navbar.component'
 export { default as Header } from './header/header.component'
+export { default as Brands } from './brands/brands.component'
+
+
+
+export {
+    slider1,
+    logo,
+    zamow,
+    mojekonto,
+    koszyk,
+    Pyszne,
+    DeliGoo,
+    uber_eats,
+    Glovo,
+    bolt_food
+} 
--- src/components/slider/imports.js
+++
@@ -1,6 +0,0 @@
-import slider1 from '../../assets/slider1.png'
-import slider2 from '../../assets/slider2.jpg'
-import slider3 from '../../assets/slider3.jpg'
-
-
-export {slider1, slider2, slider3} 
